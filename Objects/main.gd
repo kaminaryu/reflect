f7ea8648553bdbab7a068ad22c7807bb3ee68d48
IzzAmani <izzryugasaki@gmail.com>
2025-11-19 19:20:37 +0800

    Added Sonar Waves Anim

---
+++ Objects/main.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _input(evt) -> void:
+    if evt is InputEventMouseButton :
+        if evt.pressed and evt.button_index == MOUSE_BUTTON_LEFT :
+            print(evt.position);
+    # get physics space from      the world     (this is the physics space)
+            var physics_space = get_world_2d().direct_space_state
+            
+            # create a query for the ray direction using this weird ass object
+            var query := PhysicsRayQueryParameters2D.create(evt.position, evt.position + Vector2(100, 0))
+            # cast the rays
+            var result: Dictionary = physics_space.intersect_ray(query)
+            
+            pretty_print(result)
+
+func pretty_print(dict: Dictionary) -> void :
+    
+    for key in dict :
+        print(key, ": ", dict[key]);
+    
---
+++ Objects/sonar.gd
@@ -0,0 +1,74 @@
+extends Node2D
+
+const TOTAL_WAVES : int = 5
+const WAVE_FRAME_GAPS : int = 10
+const WAVE_SPEED : float = 5.5
+const WAVE_RADIUS_LIMIT : float = 100.0
+const WAVE_MAX_THICKNESS : float = 3.5
+const RIPPLE_DAMPING : float = 0.5
+
+var waveEmits := false
+var waveCenter: Vector2
+var waveFrame: Array[int]
+var waveRadius: Array[float]
+var waveThickness : Array[float]
+
+func _input(evt) -> void :
+    if evt is InputEventMouseButton :
+        if evt.pressed and evt.button_index == MOUSE_BUTTON_LEFT :
+            if waveEmits :
+                return
+            waveEmits = true
+            waveCenter = evt.position
+            waveAnim()
+            
+    
+    
+func waveAnim() -> void :
+        
+    waveFrame = []
+    waveRadius = []
+    waveThickness = []
+    for i in range(TOTAL_WAVES) :
+        waveRadius.append(-1)
+        waveFrame.append(i * WAVE_FRAME_GAPS)
+        waveThickness.append(WAVE_MAX_THICKNESS - i * RIPPLE_DAMPING)
+    
+    var frames := 0
+    var wavesFinished := 0
+    
+    print("Emitting Waves at: ", waveCenter, " with radii of ", waveRadius, " and time frames of ", waveFrame)
+    while waveEmits :
+        for i in range(len(waveFrame)) :
+            # skip the destroyed waves
+            if waveFrame[i] == -1 :
+                continue
+                
+            # if its the time to print the wave
+            if frames > waveFrame[i] :
+                waveRadius[i] += WAVE_SPEED
+                print("Wave[", i, "] Radius: ", waveRadius)
+                
+            # destroy waves
+            if waveRadius[i] > WAVE_RADIUS_LIMIT :
+                waveFrame[i] = -1
+                #waveRadius[i] = -1
+                wavesFinished += 1
+        
+        if wavesFinished >= TOTAL_WAVES :
+            waveEmits = false
+                            
+        queue_redraw()
+        frames += 1
+        await get_tree().create_timer(0.01).timeout
+        
+    
+func _draw() -> void :
+    for i in range(len(waveFrame)) :
+        # dont draw hidden or destroyed wave
+        if waveFrame[i] == -1 :
+            continue
+            
+        draw_circle(waveCenter, waveRadius[i], Color.GREEN_YELLOW, false, waveThickness[i])
+        
+    #draw_arc(waveCenter, waveRadius-10, 0, PI/2, 64, Color.RED, 10)
